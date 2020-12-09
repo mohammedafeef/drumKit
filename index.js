4d8833f292b2bd347482f2703429902bedfc8f98
mohammedafeef <mohammedafeef.maf@gmail.com>
2020-12-09 16:11:39 +0530

    added clicking animation

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const playTheMusic =(musiclink) =>{
     let music = new Audio(musiclink);
     music.play()
 }
+const doTheAnimation = (button) =>{
+    button.classList.add("blur");
+    setTimeout(()=>{
+        button.classList.remove("blur");
+    },200)
+}
 for(let i=1; i<=allDrumsFields.length; ++i){
     allDrumsFields[i-1].style.backgroundImage = `url("./images/tom${i}.png")`;
-    allDrumsFields[i-1].addEventListener("click",() =>{playTheMusic(`./sounds/tom-${i}.mp3`)});
+    allDrumsFields[i-1].addEventListener("click",() =>{
+        playTheMusic(`./sounds/tom-${i}.mp3`);
+        doTheAnimation(allDrumsFields[i-1]);
+});
 }
 document.addEventListener("keydown",(event) =>{playMusicByKeyPress(event)});
 const playMusicByKeyPress = (event) =>{
@@ -20,6 +29,8 @@ const playMusicByKeyPress = (event) =>{
     }
     let clickedLetter = event.key.toLowerCase()
     if (clickedLetter in keyForMusic) {null}else{return}
+    const currentDocument = document.querySelector(`.${clickedLetter}`);
+    doTheAnimation(currentDocument);
     let clickedKey = keyForMusic[clickedLetter];
     let MusicLinkToKey =`./sounds/tom-${clickedKey}.mp3`;
 
